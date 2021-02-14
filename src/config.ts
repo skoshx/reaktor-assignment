0b48f8f89f78c7f4d936990c95373b6157b43d7d
skoshx <rasmus.gustafsson2611@gmail.com>
2021-02-14 18:44:45 +0200

    small bugfix

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,7 @@
  */
 
 export const DEBUG = process.env.NODE_ENV !== "production";
-// export const API_ENDPOINT_BASE = "https://bad-api-assignment.reaktor.com";
-export const API_ENDPOINT_BASE = `${location.protocol}//localhost:${process.env.PORT ?? 3000}`;
+export const API_ENDPOINT_BASE = `${location.protocol}//${location.hostname}:${process.env.PORT ?? 3000}`;
 
 export const getProductsEndpoint = (category: string) => `${API_ENDPOINT_BASE}/v2/products/${category}`;
 export const getAvailabilityEndpoint = (manufacturer: string) => `${API_ENDPOINT_BASE}/v2/availability/${manufacturer}`;
